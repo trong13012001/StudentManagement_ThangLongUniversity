a58b1f4cad2668f8e468dc758f0f6662c0c82a84
trong13012001 <tronga36192@gmail.com>
2023-07-17 22:25:10 +0700

    add Screen ScoreReportCardStack,ScoreReportCardTerm

---
+++ Frontend/Student-management-ThanglongUniversity-main/Screens/Student/HomeScreen/ScoreReport/ScoreReportCard/ScoreReportCardTerm.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import {
+  Dimensions,
+} from "react-native";
+import TermList from "../../../../../components/TermList";
+
+const ScoreReportCardTerm = () => {
+
+
+  return (
+    <TermList title={"Phiếu báo điểm"} name={"Phiếu báo điểm"}/>
+  );
+};
+
+export default ScoreReportCardTerm;
+
